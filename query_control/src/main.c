7d7b763772e94ca192e1ac55928d0f93a63ec22b
Daniel <dperalta@frba.utn.edu.ar>
2025-09-10 01:06:47 -0300

    refactor - se agrega manejo de errores con goto

--- query_control/src/main.c
+++ query_control/src/main.c
@@ -4,9 +4,5 @@
 int main(int argc, char* argv[]) {
     saludar("query_control");
 
-    // SOLO PARA TEST DE FUNCIÓN start_server hardcodeo ip y puerto
-    int socket;
-    socket = start_server("127.0.0.1", "8001");
-    printf("Socket %d creado con exito!\n", socket);
     return 0;
 }
--- utils/src/utils/server.c
+++ utils/src/utils/server.c
@@ -1,6 +1,12 @@
 #include "server.h"
 
 int start_server(char *ip, char *port) {
+
+    if (ip == NULL || port == NULL) 
+    {
+        goto error;
+    }
+
     struct addrinfo hints, *server_info;
 
     // Configuramos el socket
@@ -11,42 +17,40 @@ int start_server(char *ip, char *port) {
 
     int result = getaddrinfo(ip, port, &hints, &server_info);
     if (result != 0) {
-        printf("getaddrinfo error: %s\n", gai_strerror(result));
-        return -1;
+        goto error;
     }
 
     // Creamos el socket de escucha del servidor
     int server_socket = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
     if (server_socket == -1) {
-        printf("Socket creation error\n%s\n", strerror(errno));
-        freeaddrinfo(server_info);
-        return -1;
+        goto clean;
     }
 
     // Asociamos el socket a un puerto. Verifico si se asoció correctamente
     int yes = 1;
     if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-        printf("setsockopt error\n%s\n", strerror(errno));
-        freeaddrinfo(server_info);
-        close(server_socket);
-        return -1;
+        goto clean;
     }
 
     // Bindeo el puerto al socket
     if (bind(server_socket, server_info->ai_addr, server_info->ai_addrlen) == -1) {
-        close(server_socket);
-        printf("Bind error\n%s\n", strerror(errno));
-        freeaddrinfo(server_info);
-        return -1;
+        goto bind_error;
     }
 
     // Escuchamos las conexiones entrantes
     if (listen(server_socket, SOMAXCONN) == -1) {
-        printf("Listen error\n%s\n", strerror(errno));
-        freeaddrinfo(server_info);
-        return -1;
+        goto clean;
     }
     freeaddrinfo(server_info);
 
     return server_socket;
+
+
+clean:
+    freeaddrinfo(server_info);
+bind_error:
+    close(server_socket);
+    freeaddrinfo(server_info);
+error:
+    return -1;
 }
