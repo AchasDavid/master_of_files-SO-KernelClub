19f673200b6db9d6865654fc558833d056f738ca
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2025-09-13 11:57:09 -0300

    Off by one

--- query_control/src/main.c
+++ query_control/src/main.c
@@ -16,8 +16,8 @@ int main(int argc, char* argv[])
     char* config_filepath = argv[1];
     char* query_filepath = argv[2];
     int priority = atoi(argv[3]);
-    // PATH_MAX para la ruta del query + 1 para el delimiter 0x1F + 11 para la prioridad (max int) + '\0'
-    char send_buffer[PATH_MAX+1+11];
+    // PATH_MAX para la ruta del query + 1 para el delimiter 0x1F + 11 para la prioridad (max int) + 1 para '\0'
+    char send_buffer[PATH_MAX+1+11+1];
     char response_buffer[256];
     int retval = 0;
 
@@ -75,7 +75,7 @@ int main(int argc, char* argv[])
     log_info(logger, "## Solicitud de ejecución de Query: %s, prioridad: %d", query_filepath, priority);
 
     snprintf(send_buffer, sizeof(send_buffer), "%s\x1F%d", query_filepath, priority);
-    if (send(master_socket, send_buffer, strlen(send_buffer), 0) == -1)
+    if (send(master_socket, send_buffer, strlen(send_buffer) + 1, 0) == -1)
     {
         log_error(logger, "Error al enviar el path de query y prioridad al master");
         retval = -6;
