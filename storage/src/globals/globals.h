a47b2ad867d92887c15067213cf3860c6bd6a906
David Achas <dachassilva@frba.utn.edu.ar>
2025-09-17 15:36:19 +0000

    Define storage config como variable global.

--- storage/src/globals/globals.h
+++ storage/src/globals/globals.h
@@ -3,6 +3,17 @@
 
 #include <commons/log.h>
 
+typedef struct {
+    char* storage_ip;
+    char* storage_port;
+    char* fresh_start;
+    char* module_path;
+    char* operation_delay;
+    char* block_access_delay;
+    char* log_level;
+} t_storage_config;
+
 extern t_log* g_storage_logger;
+extern t_storage_config* g_storage_config;
 
 #endif
