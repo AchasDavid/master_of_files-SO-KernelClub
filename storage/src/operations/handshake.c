be380724e0e6316683143276256af296f18230b9
David Achas <dachassilva@frba.utn.edu.ar>
2025-09-17 22:45:09 +0000

    Elimina escritura en buffer para handshake de storage a worker.

--- storage/src/operations/handshake.c
+++ storage/src/operations/handshake.c
@@ -2,9 +2,7 @@
 
 t_package* reply_handshake(void) {
 
-    t_buffer *buffer = buffer_create(sizeof(uint8_t));    
-    buffer_write_uint8(buffer, OP_CODE_RES_HANSHAKE);
-    t_package *package = package_create(OP_CODE_RES_HANSHAKE, buffer);
+    t_package *package = package_create(OP_CODE_RES_HANSHAKE, NULL);
 
     return package;
 }
