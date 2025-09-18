69530530757c07e29ad347f8bfda26fa5512f805
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2025-09-18 01:29:17 +0000

    refactor: mejorar manejo de errores en la función handshake_with_master

--- worker/src/connections/master.c
+++ worker/src/connections/master.c
@@ -7,7 +7,7 @@ int handshake_with_master(const char *master_ip, const char *master_port)
     if (client_socket_master < 0)
     {
         log_error(logger, "## No se pudo establecer conexión con Master. IP=%s:%s", master_ip, master_port);
-        return -1;
+        goto clean;
     }
 
     log_info(logger, "## Se establecio conexión con Master. IP=%s:%s", master_ip, master_port);
@@ -17,28 +17,27 @@ int handshake_with_master(const char *master_ip, const char *master_port)
     t_package *request = package_create(OP_WORKER_HANDSHAKE_REQ, buffer);
     if (package_send(request, client_socket_master) < 0) {
         log_error(logger, "## No se pudo enviar el handshake al Master. IP=%s:%s", master_ip, master_port);
-        package_destroy(request);
-        close(client_socket_master);
-        return -1;
+        goto clean;
     }
     package_destroy(request);
 
     t_package *response = package_receive(client_socket_master);
     if (response == NULL) {
         log_error(logger, "## No se pudo recibir el handshake del Master. IP=%s:%s", master_ip, master_port);
-        close(client_socket_master);
-        return -1;
+        goto clean;
     }
 
     if (response->operation_code != OP_WORKER_HANDSHAKE_RES) {
         log_error(logger, "## Handshake con Master fallido. IP=%s:%s", master_ip, master_port);
-        package_destroy(response);
-        close(client_socket_master);
-        return -1;
+        goto clean;
     }
 
     log_info(logger, "## Handshake con Master exitoso. IP=%s:%s", master_ip, master_port);
 
     package_destroy(response);
     return client_socket_master;
+clean:
+    if (request) package_destroy(request);
+    if (response) package_destroy(response);
+    return -1;
 }
