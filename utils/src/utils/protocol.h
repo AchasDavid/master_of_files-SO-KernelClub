e20407d7b368f64761979e89052fec49eee09317
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2025-09-20 03:57:46 +0000

    refactor: se quita op. codes de envio de id

--- utils/src/utils/protocol.h
+++ utils/src/utils/protocol.h
@@ -8,8 +8,6 @@ typedef enum {
     OP_QUERY_FILE_PATH,
     OP_WORKER_HANDSHAKE_REQ,
     OP_WORKER_HANDSHAKE_RES,
-    OP_WORKER_SEND_ID_REQ,
-    OP_WORKER_SEND_ID_RES,
 } t_master_op_code;
 
 typedef enum {
