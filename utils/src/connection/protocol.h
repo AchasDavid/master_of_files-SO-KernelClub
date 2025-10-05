e1292393054f991de2afa19860a3ff156fbf0eeb
Agus <agocampo@frba.utn.edu.ar>
2025-10-05 00:31:07 +0000

    Reubicacion y creacion de enums de OPs QC y Master

--- utils/src/connection/protocol.h
+++ utils/src/connection/protocol.h
@@ -5,8 +5,6 @@
 
 // Operation codes para Master
 typedef enum {
-    OP_QUERY_HANDSHAKE,
-    OP_QUERY_FILE_PATH,
     OP_WORKER_HANDSHAKE_REQ,
     OP_WORKER_HANDSHAKE_RES,
 } t_master_op_code;
@@ -15,5 +13,13 @@ typedef enum {
     STORAGE_OP_WORKER_SEND_ID_REQ,
     STORAGE_OP_WORKER_SEND_ID_RES,
 } t_storage_op_code;
+
+// Operaciones entre Master <-> Query Control
+typedef enum {
+    OP_QUERY_HANDSHAKE,
+    OP_QUERY_FILE_PATH,
+    QC_OP_READ_DATA,
+    QC_OP_MASTER_FIN 
+ } t_query_control_op_code;
 
 #endif
