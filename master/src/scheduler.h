99cc7d53dd196b6fc2ed676fcbce4db5ab734bdb
Daniel <dperalta@frba.utn.edu.ar>
2025-10-09 00:52:06 -0300

    Update: comentarios en función try_dispatch

--- master/src/scheduler.h
+++ master/src/scheduler.h
@@ -2,5 +2,11 @@
 #define SCHEDULER_H
 #include "init_master.h"
 
+/**
+ * Intenta despachar una query READY a un worker IDLE.
+ * Retorna 0 si se despachó correctamente, -1 en caso de error.
+ * Si no hay workers IDLE o queries READY, retorna 0 sin hacer nada.
+ */
 int try_dispatch(t_master* master);
+
 #endif // SCHEDULER_H
